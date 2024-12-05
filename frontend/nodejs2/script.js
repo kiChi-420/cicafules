2e53dd64bb29aa043083daee0504162fd114cdbf
ben <gomben397@hengersor.hu>
2024-12-05 13:22:59 +0100

    igazabol a mandarin ket labat novesztett

---
+++ frontend/nodejs2/script.js
@@ -0,0 +1,15 @@
+bombajo()
+function bombajo(){
+    for(let i = 0; i < 10; i++){
+        console.log("🚶🍊 <-- 💣✅");
+    }
+}
+
+//wrapper function
+//function wrapper(__dirname, __filename, require, exports, module){
+    console.log(__dirname)
+    console.log(__filename)
+    console.log(require)
+    console.log(exports)
+    console.log(module)
+//}
