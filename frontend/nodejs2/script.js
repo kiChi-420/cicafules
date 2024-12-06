0e8ecb5a59b19a96cbc2923b17f9020b4aa97511
ben <gomben397@hengersor.hu>
2024-12-06 11:28:49 +0100

    a mandarinnak mar harom laba van
    meghivja a html oldalt

--- frontend/nodejs2/script.js
+++ frontend/nodejs2/script.js
@@ -1,15 +1,30 @@
 bombajo()
 function bombajo(){
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i < 4; i++){
         console.log("🚶🍊 <-- 💣✅");
     }
 }
 
 //wrapper function
 //function wrapper(__dirname, __filename, require, exports, module){
-    console.log(__dirname)
-    console.log(__filename)
-    console.log(require)
-    console.log(exports)
-    console.log(module)
-//}
+    // console.log(__dirname)
+    // console.log(__filename)
+    // console.log(exports)
+    // console.log(require)
+    // console.log(module)
+    //}
+
+    const add = require('./calculator.js')
+    console.log(add(1, 2))
+
+    const http = require('http')
+
+    const fs = require('fs');
+    const server = http.createServer((req, res) => {
+        fs.readFile('./index.html', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'html'});
+            res.end(data);
+        });
+    })
+
+server.listen(8080);
